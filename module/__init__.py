b98fef1c79569e070a4017d15a119920a9e07e0a
LZY/我不是盘神 <77564247+Gentlesprite@users.noreply.github.com>
2024-08-05 11:20:55 +0800

    v1.0.5

--- module/__init__.py
+++ module/__init__.py
@@ -5,10 +5,10 @@
 # File:__init__.py.py
 __all__ = ['enum_define', 'meta', 'panel_form', 'process_path', 'pyrogram_extension', 'unit', 'logger_config',
            'SOFTWARE_NAME', '__version__', '__license__', '__copyright__', '__update_date__', 'SOFTWARE_FULL_NAME']
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 __license__ = "MIT License"
 __copyright__ = "Copyright (C) 2023 Gentlesprite <https://github.com/Gentlesprite>"
-__update_date__ = '2024/07/22 23:47:22'
+__update_date__ = '2024/07/05 11:16:57'
 SOFTWARE_FULL_NAME = 'Telegram Restricted Media Downloader'
 SOFTWARE_NAME = 'TRMD'
 author = 'Gentlesprite'
