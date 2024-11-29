bc3a33a7cc32aa48f7d40100ab19aa51b4e51e39
LZY/我不是盘神 <77564247+Gentlesprite@users.noreply.github.com>
2024-11-29 12:32:06 +0800

    修复nuitka打包后pygments.lexers.data缺失。

--- build.py
+++ build.py
@@ -2,7 +2,8 @@
 # Author:Gentlesprite
 # Software:PyCharm
 # Time:2024/7/22 22:37
-# File:build
+# File:build.py
+"""pip install nuitka==2.4.8"""
 import os
 from module import author, __version__, SOFTWARE_FULL_NAME
 
@@ -11,11 +12,13 @@ file_version = __version__
 ico_path = 'res/icon.ico'
 output = 'output'
 main = 'main.py'
+include_module = '--include-module=pygments.lexers.data'
 copy_right = f'Copyright (C) 2024 {author}.'
-build_command = f'nuitka --standalone --show-memory --show-progress --onefile '
+build_command = f'nuitka --standalone --show-memory --show-progress --onefile {include_module} '
 build_command += f'--output-dir={output} --file-version={file_version} '
 build_command += f'--windows-icon-from-ico="{ico_path}" '
 build_command += f'--output-filename="{app_name}.exe" --copyright="{copy_right}"  --mingw64 '
+
 build_command += main
 print(build_command)
 os.system(build_command)
