961e9761c7b727a8ee454d44fab100779214a846
LZY/我不是盘神 <77564247+Gentlesprite@users.noreply.github.com>
2024-11-29 10:53:15 +0800

    在填写配置文件之前，加入提示。

--- module/__init__.py
+++ module/__init__.py
@@ -14,7 +14,42 @@ from ctypes import windll
 from loguru import logger
 from pyrogram import utils
 from rich.console import Console
+from rich.markdown import Markdown
 from typing import Tuple, List, Dict, Any, Optional
+
+
+def print_helper():
+    config_helper = r"""
+# 配置文件说明
+```yaml
+# 下载完成直接打开软件即可,软件会一步一步引导你输入的!这里只是介绍每个参数的含义
+# 填入第一步教你申请的api_hash和api_id
+# 如果是按照软件的提示填,不需要加引号,如果是手动打开config.yaml修改配置,请仔细阅读下面内容
+# 手动填写注意区分冒号类型,例如 - 是:不是：
+# 手动填写的时候还请注意参数冒号不加空格会报错 后面有一个空格,例如 - api_hash: xxx而不是api_hash:xxx
+api_hash: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx #api_hash没有引号
+api_id: 'xxxxxxxx' #注意配置文件中只有api_id有引号！！！
+links: D:\path\where\your\link\txt\save\content.txt # 链接地址写法如下:
+# 新建txt文本,一个链接为一行,将路径填入即可请不要加引号,在软件运行前就准备好
+# D:\path\where\your\link\txt\save\content.txt 请注意一个链接一行
+# 列表写法已在v1.1.0版本中弃用,目前只有上述唯一写法！！！
+# 不要存在中文或特殊字符
+max_download_task: 3 # 最大的同时下载任务数 注意:如果你不是Telegram会员,那么最大同时下载数只有1
+proxy: # 代理部分,如不使用请全部填null注意冒号后面有空格,否则不生效导致报错!
+  enable_proxy: true # 是否开启代理 true为开启 false为关闭
+  hostname: 127.0.0.1 # 代理的ip地址
+  is_notice: false # 是否开启代理提示, true为每次打开询问你是否开启代理, false则为关闭
+  scheme: socks5 # 代理的类型,支持http,socks4,socks5
+  port: 10808 # 代理ip的端口
+  username: null # 代理的账号,有就填,没有请都填null!
+  password: null # 代理的密码,有就填,没有请都填null!
+save_path: F:\path\the\media\where\you\save # 下载的媒体保存的地址,没有引号,不要存在中文或特殊字符
+# 再次提醒,由于nuitka打包的性质决定,中文路径无法被打包好的二进制文件识别,故在配置文件时无论是链接路径还是媒体保存路径都请使用英文命名。
+is_shutdown: true # 是否下载完成后自动关机 true为下载完成后自动关机 false为下载完成后不关机
+```
+"""
+    markdown = Markdown(config_helper)
+    console.print(markdown)
 
 
 # v1.1.2 解决链接若附带/c字段即私密频道无法下载的问题,是由于pyrogram的问题:https://github.com/pyrogram/pyrogram/issues/1314
@@ -68,3 +103,4 @@ read_input_history(history_path=INPUT_HISTORY_PATH, max_record_len=MAX_RECORD_LE
 logger.add(sink=LOG_PATH, level='INFO', rotation='10 MB', retention='10 days', compression='zip', encoding='UTF-8',
            enqueue=True)
 CustomDumper.add_representer(type(None), CustomDumper.represent_none)
+print_helper()
