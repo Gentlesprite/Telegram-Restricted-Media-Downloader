796280768278210c7a0a5eabae51d76e9d16b5cb
LZY/我不是盘神 <77564247+Gentlesprite@users.noreply.github.com>
2025-01-19 22:23:12 +0800

    v1.2.9 修复已知问题。

--- module/__init__.py
+++ module/__init__.py
@@ -14,139 +14,13 @@ import platform
 import datetime
 import pyrogram
 import mimetypes
-from pyrogram.errors import FloodWait
-from pyrogram.errors.exceptions.bad_request_400 import MsgIdInvalid, UsernameInvalid
-from pyrogram.errors.exceptions.unauthorized_401 import SessionRevoked, AuthKeyUnregistered, SessionExpired
-from ctypes import windll
+
+from pyrogram import utils
 from rich.console import Console
 from rich.logging import RichHandler
-from logging.handlers import RotatingFileHandler
-from pyrogram import utils
 from typing import Set, Dict, Any, Optional
-
-
-class TelegramRestrictedMediaDownloaderClient(pyrogram.Client):
-
-    async def authorize(self) -> pyrogram.types.User:
-        if self.bot_token:
-            return await self.sign_in_bot(self.bot_token)
-        console.print(
-            f'Pyrogram is free software and comes with ABSOLUTELY NO WARRANTY. Licensed\n'
-            f'under the terms of the {pyrogram.__license__}.')
-        console.print(
-            f'欢迎使用[#b4009e]{SOFTWARE_FULL_NAME}[/#b4009e](版本 {__version__})'
-            f'基于Pyrogram(版本 {pyrogram.__version__})。')
-        while True:
-            try:
-                if not self.phone_number:
-                    while True:
-                        value = console.input('请输入「电话号码」或「bot token」([#6a2c70]电话号码[/#6a2c70]需以[#b83b5e]「+地区」'
-                                              '[/#b83b5e]开头!如:[#f08a5d]+86[/#f08a5d][#f9ed69]15000000000[/#f9ed69]):').strip()
-                        if not value:
-                            continue
-
-                        confirm = console.input(
-                            f'所输入的「{value}」是否[green]正确[/green]? - 「y|n」(默认y): ').strip().lower()
-
-                        if confirm == 'y' or confirm == '':
-                            break
-                        else:
-                            log.warning(f'意外的参数:"{confirm}",支持的参数 - 「y|n」(默认y)')
-                    if ":" in value:
-                        self.bot_token = value
-                        return await self.sign_in_bot(value)
-                    else:
-                        self.phone_number = value
-
-                sent_code = await self.send_code(self.phone_number)
-            except pyrogram.errors.BadRequest as e:
-                console.print(e.MESSAGE)
-                self.phone_number = None
-                self.bot_token = None
-            else:
-                break
-
-        sent_code_descriptions = {
-            pyrogram.enums.SentCodeType.APP: 'Telegram app',
-            pyrogram.enums.SentCodeType.SMS: 'SMS',
-            pyrogram.enums.SentCodeType.CALL: 'phone call',
-            pyrogram.enums.SentCodeType.FLASH_CALL: 'phone flash call',
-            pyrogram.enums.SentCodeType.FRAGMENT_SMS: 'Fragment SMS',
-            pyrogram.enums.SentCodeType.EMAIL_CODE: 'email code'
-        }
-
-        console.print(f'「验证码」已通过「{sent_code_descriptions[sent_code.type]}」发送。')
-
-        while True:
-            if not self.phone_code:
-                self.phone_code = console.input('请输入收到的「验证码」:').strip()
-
-            try:
-                signed_in = await self.sign_in(self.phone_number, sent_code.phone_code_hash, self.phone_code)
-            except pyrogram.errors.BadRequest as e:
-                console.print(e.MESSAGE)
-                self.phone_code = None
-            except pyrogram.errors.SessionPasswordNeeded as e:
-                console.print(e.MESSAGE)
-
-                while True:
-                    console.print('密码提示:{}'.format(await self.get_password_hint()))
-
-                    if not self.password:
-                        self.password = console.input('输入「密码」(为空代表恢复密码):', password=self.hide_password).strip()
-
-                    try:
-                        if not self.password:
-                            confirm = console.input('确认「恢复密码」? - 「y|n」(默认y):').strip().lower()
-
-                            if confirm == 'y' or confirm == '':
-                                email_pattern = await self.send_recovery_code()
-                                console.print(f'「恢复代码」已发送到「{email_pattern}」。')
-
-                                while True:
-                                    recovery_code = console.input('请输入「恢复代码」:').strip()
-
-                                    try:
-                                        return await self.recover_password(recovery_code)
-                                    except pyrogram.errors.BadRequest as e:
-                                        console.print(e.MESSAGE)
-                                    except Exception as e:
-                                        console.print_exception()
-                                        raise
-                            else:
-                                self.password = None
-                        else:
-                            return await self.check_password(self.password)
-                    except pyrogram.errors.BadRequest as e:
-                        console.print(e.MESSAGE)
-                        self.password = None
-            else:
-                break
-
-        if isinstance(signed_in, pyrogram.types.User):
-            return signed_in
-
-        while True:
-            first_name = console.input('输入「名字」:').strip()
-            last_name = console.input('输入「姓氏」(为空代表跳过): ').strip()
-
-            try:
-                signed_up = await self.sign_up(
-                    self.phone_number,
-                    sent_code.phone_code_hash,
-                    first_name,
-                    last_name
-                )
-            except pyrogram.errors.BadRequest as e:
-                console.print(e.MESSAGE)
-            else:
-                break
-
-        if isinstance(signed_in, pyrogram.types.TermsOfService):
-            console.print('\n' + signed_in.text + '\n')
-            await self.accept_terms_of_service(signed_in.id)
-
-        return signed_up
+from logging.handlers import RotatingFileHandler
+from pyrogram.errors.exceptions.unauthorized_401 import SessionRevoked, AuthKeyUnregistered, SessionExpired
 
 
 # v1.1.2 解决链接若附带/c字段即私密频道无法下载的问题,是由于pyrogram的问题:https://github.com/pyrogram/pyrogram/issues/1314
@@ -172,19 +46,6 @@ def read_input_history(history_path: str, max_record_len: int) -> None:
     atexit.register(readline.write_history_file, history_path)
 
 
-def check_run_env() -> bool:  # 检测是控制台运行还是IDE运行
-    return windll.kernel32.SetConsoleTextAttribute(windll.kernel32.GetStdHandle(-0xb), 0x7)
-
-
-def get_platform() -> str:
-    if platform.system() == 'Windows':
-        return 'Windows'
-    elif platform.system() == 'Linux':
-        return 'Linux'
-    else:
-        return 'Unknown'
-
-
 # 自定义 yaml文件中 None 的表示
 class CustomDumper(yaml.Dumper):
     def represent_none(self, data):
@@ -193,9 +54,9 @@ class CustomDumper(yaml.Dumper):
 
 console = Console(log_path=False)
 utils.get_peer_type = get_peer_type_new
-__version__ = '1.2.8'
+__version__ = '1.2.9'
 __license__ = 'MIT License'
-__update_date__ = '2025/01/14 21:22:33'
+__update_date__ = '2025/01/19 20:06:23'
 __copyright__ = f'Copyright (C) 2024-{__update_date__[:4]} Gentlesprite <https://github.com/Gentlesprite>'
 SOFTWARE_FULL_NAME = 'Telegram Restricted Media Downloader'
 SOFTWARE_NAME = 'TRMD'
